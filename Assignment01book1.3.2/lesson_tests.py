5e9f3906b61cab1aefcb9abe4c439e3be93291e3
PeterdHapke <phapke@marshalltown.k12.ia.us>
2017-10-19 13:10:08 -0500

    repl long test5

--- Assignment01book1.3.2/lesson_tests.py
+++ Assignment01book1.3.2/lesson_tests.py
@@ -1,8 +1,8 @@
-class UnitTests(unittest.TestCase):
+class unittests132(unittest.TestCase):
  def test_mean1(self):
      msg = "your mean(a, b, c) failed because there was a problem with the syntax or there was a problem with your math"
 
-     #self.assertEquals(mean(6, 6, 6), 6)
+     self.assertEquals(mean(6, 6, 6), 6)
      self.assertEquals(mean(9, 10, 11), 10)
      return True
 
