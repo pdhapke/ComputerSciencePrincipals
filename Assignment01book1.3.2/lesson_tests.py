36e035be1ba56ae6626f98fa6eeb747be767cb76
PeterdHapke <phapke@marshalltown.k12.ia.us>
2017-10-19 15:58:01 -0500

    repl slow test 11

--- Assignment01book1.3.2/lesson_tests.py
+++ Assignment01book1.3.2/lesson_tests.py
@@ -1,7 +1,4 @@
 class UnitTests(unittest.TestCase):
+    def test_mean1(self):
+        msg = "your mean(a, b, c) failed because there was a problem with the syntax or there was a problem with your math"
 
- def test_mean1(self):
-     msg = "your mean(a, b, c) failed because there was a problem with the syntax or there was a problem with your math"
-     self.assertEquals(mean(6, 6, 6), 6)
-     #self.assertEquals(mean(9, 10, 11), 10)
-     #return True
